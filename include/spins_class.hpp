ab3524c128bd51d3720ea2bf0bba1d213171214b
Jonathon Waters <jmw2g14@soton.ac.uk>
2017-06-20 15:51:07 +0100

    added spin class destructor

--- include/spins_class.hpp
+++ include/spins_class.hpp
@@ -6,7 +6,9 @@ class spin_lattice_1d
 public:
     double* thetas;
     double* phis;
+    int N;
     spin_lattice_1d(int length);
+    ~spin_lattice_1d();
 }
 
 #endif
--- lib/spins_class.cpp
+++ lib/spins_class.cpp
@@ -5,4 +5,11 @@ spin_lattice_1d::spin_lattice_1d(int length)
 {
     thetas = alloc_1darr(length);
     phis = alloc_1darr(length);
+    N = length;
+}
+
+spin_lattice_1d::~spin_lattice_1d()
+{
+    dealloc_1darr(thetas);
+    dealloc_1darr(phis);
 }
