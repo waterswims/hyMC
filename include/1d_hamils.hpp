484f7845d018c01067a7c75f9ccac1fa0d6568e8
Jonathon Waters <j.m.waters@soton.ac.uk>
2017-06-21 21:09:59 +0100

    closes #35

--- include/1d_hamils.hpp
+++ include/1d_hamils.hpp
@@ -3,6 +3,10 @@
 
 #include "spins_class.hpp"
 
+double total_energy_1d(const spin_lattice_1d &spins,
+    const spin_lattice_1d &vels,
+    const bool* energy_flags);
+
 double exchange_1d(const spin_lattice_1d &spins);
 
 double kinetic_1d(const spin_lattice_1d &vels);
