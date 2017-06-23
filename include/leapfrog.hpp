39f493caa564a01583e6a85d2d8856dd18677a0a
owlas <ollielaslett@icloud.com>
2017-06-23 11:16:56 +0100

    valarray in leapfrog

--- include/leapfrog.hpp
+++ include/leapfrog.hpp
@@ -1,33 +1,31 @@
 #ifndef LEAPFROG_H
 #define LEAPFROG_H
 #include <functional>
+#include <valarray>
 
 namespace leapfrog {
 
     /// New velocity of the system states after half a step
     void half_step_velocity(
-        double *new_vels,
-        const double *vels,
-        const double *energy_gads,
-        const size_t system_size,
+        std::valarray<double> &new_vels,
+        const std::valarray<double> &vels,
+        const std::valarray<double> &energy_gads,
         const double eps );
 
     /// New states of the system after a full step
-    void step_state( double *new_state,
-                     const double *state,
-                     const double *vels,
-                     const size_t system_size,
+    void step_state( std::valarray<double> &new_state,
+                     const std::valarray<double> &state,
+                     const std::valarray<double> &vels,
                      const double eps );
 
     /// New state and velocity of the system after one leap frog step
     void lfs(
-        double *new_state,
-        double *new_velocity,
-        double *energy_grads_work,
-        const double *state,
-        const double *velocity,
-        const std::function<void(double*,const double*, const size_t)> &energy_grads,
-        const size_t system_size,
+        std::valarray<double> &new_state,
+        std::valarray<double> &new_velocity,
+        std::valarray<double> &energy_grads_work,
+        const std::valarray<double> &state,
+        const std::valarray<double> &velocity,
+        const std::function<void(std::valarray<double>&,const std::valarray<double> &)> &energy_grads,
         const double eps );
 
 } // end namespace
--- lib/leapfrog.cpp
+++ lib/leapfrog.cpp
@@ -1,47 +1,40 @@
 #include "../include/leapfrog.hpp"
 
 void leapfrog::half_step_velocity(
-    double *new_vels,
-    const double *vels,
-    const double *energy_grads,
-    const size_t N,
+    std::valarray<double> &new_vels,
+    const std::valarray<double> &vels,
+    const std::valarray<double> &energy_grads,
     const double eps )
 {
-    #pragma simd
-    for( unsigned int n=0; n<N; n++ )
-        new_vels[n] = vels[n] - eps/2.0 * energy_grads[n];
+    new_vels = vels - eps / 2.0 * energy_grads;
 }
 
 void leapfrog::step_state(
-    double *new_state,
-    const double *state,
-    const double *vels,
-    const size_t N,
+    std::valarray<double> &new_state,
+    const std::valarray<double> &state,
+    const std::valarray<double> &vels,
     const double eps )
 {
-    #pragma simd
-    for( unsigned int n=0; n<N; n++ )
-        new_state[n] = state[n] + eps * vels[n];
+    new_state = state + eps * vels;
 }
 
 void leapfrog::lfs(
-    double *new_state,
-    double *new_velocity,
-    double *energy_grads_work,
-    const double *state,
-    const double *velocity,
-    const std::function<void(double*,const double*,const size_t)> &energy_grads,
-    const size_t N,
+    std::valarray<double> &new_state,
+    std::valarray<double> &new_velocity,
+    std::valarray<double> &energy_grads_work,
+    const std::valarray<double> &state,
+    const std::valarray<double> &velocity,
+    const std::function<void(std::valarray<double>&,const std::valarray<double>&)> &energy_grads,
     const double eps )
 {
     // Compute the gradient of the energy landscape
-    energy_grads( energy_grads_work, state, N );
+    energy_grads( energy_grads_work, state);
 
     // Compute the half-step velocity and next state
-    half_step_velocity( new_velocity, velocity, energy_grads_work, N, eps );
-    step_state( new_state, state, new_velocity, N, eps );
+    half_step_velocity( new_velocity, velocity, energy_grads_work, eps );
+    step_state( new_state, state, new_velocity, eps );
 
     // Finish by computing the full-step velocity
-    energy_grads( energy_grads_work, new_state, N );
-    half_step_velocity( new_velocity, new_velocity, energy_grads_work, N, eps );
+    energy_grads( energy_grads_work, new_state );
+    half_step_velocity( new_velocity, new_velocity, energy_grads_work, eps );
 }
