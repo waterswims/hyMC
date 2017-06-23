0e84c9eff433316fac8c631b9a07fae67de88b3e
owlas <ollielaslett@icloud.com>
2017-06-23 11:16:56 +0100

    valarray in leapfrog

--- tests/leapfrog_test.hpp
+++ tests/leapfrog_test.hpp
@@ -3,23 +3,24 @@
 #include "../include/leapfrog.hpp"
 #include "./googletest/googletest/include/gtest/gtest.h"
 #include <cmath>
+#include <valarray>
 
 TEST( leapfrog, half_step_velocity )
 {
     // Initial velocity
-    double v[2] = {0.12, 0.27};
+    std::valarray<double> v = {0.12, 0.27};
 
     // energy gradient
     // U = 4x ** 2 + x * sin(y)
     // dux = 8x + sin(y)
     // duy = x * cos(y)
     // let x=2, y=5
-    double grad[2] = {8*2 + std::sin(5), 2*std::cos(5)};
+    std::valarray<double> grad = {8*2 + std::sin(5), 2*std::cos(5)};
     double eps=0.2;
 
     // compute half step velocity
-    double vnew[2];
-    leapfrog::half_step_velocity( vnew, v, grad, 2, eps);
+    std::valarray<double> vnew( 2 );
+    leapfrog::half_step_velocity( vnew, v, grad, eps);
 
     EXPECT_DOUBLE_EQ( -1.3841075725336864, vnew[0] );
     EXPECT_DOUBLE_EQ( 0.21326756290735477, vnew[1] );
@@ -28,15 +29,15 @@ TEST( leapfrog, half_step_velocity )
 TEST( leapfrog, step_state )
 {
     // Initial state
-    double x[2] = {1, 2};
+    std::valarray<double> x = {1, 2};
 
     // Velocity and step size
-    double vel[2] = {0.2, -0.7};
+    std::valarray<double> vel = {0.2, -0.7};
     double eps = 0.5;
 
     // Compute the next state
-    double xnew[2];
-    leapfrog::step_state( xnew, x, vel, 2, eps );
+    std::valarray<double> xnew( 2 );
+    leapfrog::step_state( xnew, x, vel, eps );
 
     EXPECT_DOUBLE_EQ( 1.1, xnew[0] );
     EXPECT_DOUBLE_EQ( 1.65, xnew[1] );
@@ -45,8 +46,8 @@ TEST( leapfrog, step_state )
 TEST( leapfrog, lfs )
 {
     // Initial state and velocity
-    double x[2] = {2, 5};
-    double v[2] = {0.12, 0.27};
+    std::valarray<double> x = {2, 5};
+    std::valarray<double> v = {0.12, 0.27};
 
     // step size
     double eps=0.2;
@@ -55,16 +56,16 @@ TEST( leapfrog, lfs )
     // U = 4x ** 2 + x * sin(y)
     // dux = 8x + sin(y)
     // duy = x * cos(y)
-    std::function<void(double*,const double*,const size_t)> f_grad =
-        [](double * out, const double * in, const size_t)
+    std::function<void(std::valarray<double>&,const std::valarray<double>&)> f_grad =
+        [](std::valarray<double>& out, const std::valarray<double>& in)
         {
             out[0] = 8*in[0] + std::sin( in[1] );
             out[1] = in[0] * std::cos( in[1] );
         };
 
     // compute the next velocity and state
-    double xnew[2], vnew[2], work[2];
-    leapfrog::lfs( xnew, vnew, work, x, v, f_grad, 2, eps );
+    std::valarray<double> xnew( 2 ), vnew( 2 ), work( 2 );
+    leapfrog::lfs( xnew, vnew, work, x, v, f_grad, eps );
 
     ASSERT_DOUBLE_EQ( 1.7231784854932628, xnew[0] );
     ASSERT_DOUBLE_EQ( 5.0426535125814711, xnew[1] );
