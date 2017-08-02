1b9edf7f39a021b5ad6a3007ab2c6962a435bb0b
Jonathon Waters <jmw2g14@soton.ac.uk>
2017-08-02 13:50:54 +0100

    closes #74 zeeman gradient

--- include/all_hamils.hpp
+++ include/all_hamils.hpp
@@ -9,7 +9,7 @@ namespace hmc
     ///////////////////////////////////////////////////////////////////////////
     /// \brief Calculates the zeeman energy of a system.
     ///
-    /// Calculates the standard Heisenberg exchange energy \f$ H \sum_{i}
+    /// Calculates the standard zeeman energy \f$ H \sum_{i}
     /// \mathbf{s}_i\f$ where \f$H =\f$ is actually the field strength
     /// multiplied by the moment of an atom.
     ///
@@ -22,6 +22,26 @@ namespace hmc
     /// \param H The field strength multiplied by the moment of an atom
     ///////////////////////////////////////////////////////////////////////////
     double zeeman_energy(const std::vector<std::valarray<double> >& trig_angles, double H);
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief Calculates the gradient of the zeeman energy of a system.
+    ///
+    /// Calculates the gradient \f$\partial/\partial \theta\f$ and
+    /// \f$\partial/\partial \phi\f$ of the standard zeeman energy \f$ H \sum_{i}
+    /// \mathbf{s}_i\f$ where \f$H =\f$ is actually the field strength
+    /// multiplied by the moment of an atom.
+    ///
+    /// \param trig_angles Vector of the trigonometric transformations of the
+    ///                    angles of the spins ordered as: cos(theta),
+    ///                    sin(theta), cos(phi), sin(phi), each angle given as
+    ///                    current, left, right, up, down.
+    /// \param grad_out Reference to the valarray where the output gradient
+    ///                 will be stored. The gradient is stored as the element
+    ///                 wise derivitive of data.
+    /// \param H The field strength multiplied by the moment of an atom
+    ///////////////////////////////////////////////////////////////////////////
+    void zeeman_grad(std::valarray<double>& grad_out,
+        const std::vector<std::valarray<double> >& trig_angles, double H);
 }
 
 #endif
--- tests/all_hamils_tests.hpp
+++ tests/all_hamils_tests.hpp
@@ -23,7 +23,39 @@ TEST(Hamiltonian_Gen, Zeeman)
 
     std::vector<std::valarray<double> > E_input = hmc::trig_left_up(test_spins);
 
-    EXPECT_NEAR(hmc::zeeman_energy(E_input, H), 0.5827041377, 0.5827041377*1e-9);
+    EXPECT_NEAR(hmc::zeeman_energy(E_input, H), -0.5827041377, 0.5827041377*1e-9);
+}
+
+TEST(Hamiltonian_Gen, Zeeman_Grad)
+{
+    double H = 2.1;
+    int size = 4;
+    std::valarray<double> test_spins(size*2);
+
+    test_spins[0] = 0.3;
+    test_spins[4] = 2.3;
+
+    test_spins[1] = 1.6;
+    test_spins[5] = 0.1;
+
+    test_spins[2] = 5.2;
+    test_spins[6] = 1.1;
+
+    test_spins[3] = 3.4;
+    test_spins[7] = 2.1;
+
+    std::valarray<double> grad(size*2);
+    grad = 0;
+    std::vector<std::valarray<double> > g_input = hmc::trig_lrud(test_spins);
+    hmc::zeeman_grad(grad, g_input, H);
+    for (int i = 0; i < 4; i++)
+    {
+        EXPECT_EQ(grad[i], 0);
+    }
+    EXPECT_NEAR(grad[4], H*0.7457052122, H*0.7457052122*1e-9);
+    EXPECT_NEAR(grad[5], H*0.09983341665, H*0.09983341665*1e-9);
+    EXPECT_NEAR(grad[6], H*0.8912073601, H*0.8912073601*1e-9);
+    EXPECT_NEAR(grad[7], H*0.8632093666, H*0.8632093666*1e-9);
 }
 
 #endif
